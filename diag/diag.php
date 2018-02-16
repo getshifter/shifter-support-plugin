b2ac5ff1bb769223cc716776a1fe179d379461ce
sawanoboly <sawanoboriyu@higanworks.com>
2018-02-16 14:51:53 +0900

    remove Send to Shifter from histroy

--- diag/diag.php
+++ diag/diag.php
@@ -8,7 +8,7 @@ function link_to($url) {
 <div class="shifter-diag-wrap">
 
 <nav class="system-report-nav">
-  <a href="#" id="shifter-support-diag-copy" class="button">View System Report</a>
+  <a href="#" id="shifter-support-diag-copy" class="button">Copy to your clipboard</a>
 </nav>
 
 <div aria-hidden="true" id="shifter-debug-meta" class="shifter-debug-meta">
@@ -16,9 +16,30 @@ function link_to($url) {
   $theme_data = (array) wp_get_theme();
   $theme = wp_get_theme();
   $plugins = (array) get_option("active_plugins");
-  $system_report = array_merge($theme_data, $plugins);
-  $system_report = json_encode($system_report);
-  echo $system_report; ?>
+  $system_report = array(
+    'theme' => array(
+      'Name' => $theme->get('Name'),
+      'ThemeURI' => $theme->get('ThemeURI'),
+      'TextDomain' => $theme->get('TextDomain'),
+      'Version' => $theme->get('Version'),
+    )
+  );
+  $plugins_report = array();
+  foreach($plugins as $plugin) {
+    $plugin_meta = get_plugin_data(WP_PLUGIN_DIR . "/" . $plugin);
+    $plugins_report[] = array(
+      'Name' => $plugin_meta['Name'],
+      'PluginURI' => $plugin_meta['PluginURI'],
+      'TextDomain' => $plugin_meta['TextDomain'],
+      'Version' => $plugin_meta['Version'],
+    );
+  };
+  $system_report = array_merge(
+    $system_report,
+    array("active_plugins" => $plugins_report)
+  );
+  $system_report = json_encode($system_report, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
+  echo trim($system_report); ?>
 </div>
 
 <div id="shifter-support-diag" class="shifter-support-diag">
