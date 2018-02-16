de42b9424ae223f3aa42c77735fd9255a9a67f78
sawanoboly <sawanoboriyu@higanworks.com>
2018-02-16 15:06:29 +0900

    bump 1.1.4

--- shifter-support-plugin.php
+++ shifter-support-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Shifter Support Plugin
 Plugin URI: https://github.com/getshifter/shifter-support-plugin
 Description: This plugin is provided access to Shifter support.
-Version: 1.1.3
+Version: 1.1.4
 Author: Shifter Team
 Author URI: https://getshifter.io
 License: GPL2
